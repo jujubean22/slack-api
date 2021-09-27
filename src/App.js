c9695ac030fa161c57db79ff506358d64a861659
jujubean22 <jmevillanueva22@gmail.com>
2021-09-27 12:13:58 +0800

    Added initial  UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Router>
+        <>
+          <Header />
+          <Appbody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* Chat */}
+              </Route>
+            </Switch>
+          </Appbody>
+        </>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const Appbody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
