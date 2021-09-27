45461fc0348ed530c7e03943e310c5aefc898edb
jujubean22 <jmevillanueva22@gmail.com>
2021-09-27 17:17:41 +0800

    added useState Hook for user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar/Sidebar";
 import SignUp from "./components/Login/SignUp";
-let user = false
 
 function App() {
+  const [user, setUser] = useState(false);
   return (
     <div className="app">
       <Router>
