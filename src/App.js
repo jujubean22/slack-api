a4419d1dbdc2b3eba94c5942e4d165bd10f05d04
jujubean22 <jmevillanueva22@gmail.com>
2021-09-28 18:31:07 +0800

    added registration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import styled from "styled-components";
 import Login from "./components/Login/Login"; 
 import Home from './components/Profile/Home';
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path='/slack-api' component={Login} />
           <Route exact path='/home' component={Home} />
+          <Route exact path='/register' component={Register} />
         </Switch>
       </Router>
     </div>
