2cdfe16e9e034c5f2d2aca731c8deaebec045fea
Joseph <ralphlsulit@gmail.com>
2021-10-01 12:18:40 +0800

    added search box component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/Login"; 
+import Login from "./components/Login/Login"; 
 import Home from './components/Profile/Home';
 import Register from "./components/Register/Register";
 
