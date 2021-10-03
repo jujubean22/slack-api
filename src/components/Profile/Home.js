5a21c9693f2e71e9baf15d67945dd7b1df3092d7
Kobato13 <johncarlomartinez13@gmail.com>
2021-10-03 16:49:42 +0800

    Added get channel api in Home.js

--- src/components/Profile/Home.js
+++ src/components/Profile/Home.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
 import styled from "styled-components";
 import Homepage from './Homepage';
 import Chat from '../Chats/Chat';
+import axios from 'axios'
 
 function Home({loginData}) {
+
+  const [userHeaders, setUserHeaders] = useState("");
+  const [isRender, setIsRender] = useState(false);
+
+  const handleIsRender = () => {
+    setIsRender(!isRender);
+  }
+
+  useEffect(() => {
+
+    const headers = {
+      token: loginData.headers["access-token"],
+      client: loginData.headers.client,
+      expiry: loginData.headers.expiry,
+      uid: loginData.headers.uid,
+    };
+
+    setUserHeaders(headers)
+
+    const { token, client, expiry, uid  } = headers
+
+    axios.get("http://206.189.91.54//api/v1/channels",
+    {
+      headers:{
+        "access-token": token,
+        "client": client,
+        "expiry": expiry,
+        "uid": uid,
+      },
+    })
+    .then(res => {
+      console.log("Channel render:", res);
+    })
+    .catch(err => console.log("Error Getting Channel: ", err))
+
+  }, [isRender]);
+  
   return (
     <div>
       <Router>
@@ -18,7 +56,7 @@ function Home({loginData}) {
               <Homepage />
             </Route> */}
             <Route path='/'>
-              <Chat loginData={loginData}/>
+              <Chat loginData={loginData} headers={userHeaders} handleIsRender={handleIsRender} />
             </Route>
             </Switch>
           </Appbody>
