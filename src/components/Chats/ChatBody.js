dbdd082e66090c44740407632130f306be672c8f
Joseph <ralphlsulit@gmail.com>
2021-10-04 19:09:06 +0800

    chat body component

--- src/components/Chats/ChatBody.js
+++ src/components/Chats/ChatBody.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 function ChatBody({data}) {
 
-  const { body, created_at, sender: {id, email} } = data
+  const { body, created_at, sender: { id, email } } = data;
 
   return (
     <div>
-      {body}
     </div>
   );
 };
