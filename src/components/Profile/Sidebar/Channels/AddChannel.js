657763a3be1e9768737cc0e7d8a264fb628a4bb4
Kobato13 <johncarlomartinez13@gmail.com>
2021-10-11 16:45:05 +0800

    Added 'Add Channel' functionality, will add the add members as well

--- src/components/Profile/Sidebar/Channels/AddChannel.js
+++ src/components/Profile/Sidebar/Channels/AddChannel.js
@@ -1,126 +1,42 @@
 import styled from 'styled-components'
-import axios from "axios"
 import React, { useState} from 'react';
-import { Link } from "react-router-dom";
+import { addChannel } from "../../../../api/API"
 
-function AddChannel({loginData, headers, handleIsRender }) {
-const [addChannel, setAddChannel] = useState('')
-const [usersDataArray, setUsersDataArray] = useState([])
-const [userList, setUserList] = useState([])
-const [newMember, setNewMember] = useState('')
-  
-//calling add member in a created channel modal
-  // const [toggleAddChannelMember, setToggleAddChannelMemberyar] = useState(false)
+function AddChannel({loginData, headers, handleToggleAddChannel }) {
 
-  const getAllUsers = () => {
-    const {token, client, expiry, uid} = headers
+  //state
+  const [channelName, setChannelName] = useState('')
+  const [errorWarning, setErrorWarning] = useState(false)
+  const [errorText, setErrorText] = useState('')
+  const [usersDataArray, setUsersDataArray] = useState([])
 
-    axios.get("http://206.189.91.54//api/v1/users", 
-    {
-      headers:{
-        "access-token": token,
-        "client": client,
-        "expiry": expiry,
-        "uid": uid,
-      }
-    })
-    .then(res => {
-      setUserList(res.data.data)
-      console.log(userList)
-    })
-    .catch(error => error)
+  //channel name value
+  const handleChannelInput = (e) => {
+    setChannelName(e.target.value);
   }
 
   const createChannel = (e) => {
-  const newMembers = userList.find( u => u.uid === newMember)
-  e.preventDefault();
+    e.preventDefault();
 
-    setUsersDataArray([loginData.data.data.id])
-    console.log(usersDataArray);
+    setUsersDataArray(loginData.data.data.id)
 
-  
-    const addNewChannel = {
-      name: addChannel,
-      user_ids: usersDataArray,
+    const addNewChannelObj = {
+      name: channelName,
+      user_ids: [usersDataArray],
       headers: headers,
     }  
 
-    const { name, user_ids, headers:{ token, client, expiry, uid } } = addNewChannel
-
-    axios.post('http://206.189.91.54//api/v1/channels',
-    {
-      name,
-      user_ids
-    },
-    {
-      headers:{
-        "access-token": token,
-        "client": client,
-        "expiry": expiry,
-        "uid": uid,
+    addChannel(addNewChannelObj)
+    .then(res => {
+      if(res.data.errors[0] !== null) {
+        setErrorText(res.data.errors[0])
+        setErrorWarning(true)
+      } else {
+        console.log("successfully added", res)
+        setErrorWarning(false)
       }
     })
-    .then(res => {
-      console.log(res)
-    })
     .catch(error => error)
-
-
-    axios.get('http://206.189.91.54//api/v1/channels',
-    {
-      headers:{
-        "access-token": token,
-        "client": client,
-        "expiry": expiry,
-        "uid": uid,
-      }
-    })
-    .then(res => {
-      console.log(res)
-    })
-    .catch(error => error)
-    handleIsRender()
-  }
-
-  const handleGetChannel = (e) => {
-    e.preventDefault();
-    setAddChannel(e.target.value);
-    getAllUsers()
-  }
-
-  // const AddMemberChannel =(e) =>{
-  //   e.preventDefault()
-  //   const newMembers = userList.find( u => u.uid === newMember)
-  //   let newMembersId = newMembers.id
-  //   console.log(newMembers.id)
-
-  //     const addingNewMember = {
-  //     id: newMembersId,
-  //     headers: headers,
-  //   }
-  //   const { id, member_id, headers:{token, client, expiry, uid}} = addingNewMember
-
-  //   axios.post('http://206.189.91.54//api/v1/channel/add_member',
-  //   {
-  //     id,
-  //     member_id
-  //   },
-  //   {
-  //     headers:{
-  //       "access-token": token,
-  //       "client": client,
-  //       "expiry": expiry,
-  //       "uid": uid,
-  //     }
-  //   })
-  //   .then(res => {
-  //     console.log(res)
-  //   })
-  //   .catch(error => error)
-  // }
-
-  const handleAddMemberChannel = (e) => {
-    setNewMember(e.target.value)
   }
 
   return (
@@ -129,19 +45,14 @@ const [newMember, setNewMember] = useState('')
         <AddChannelForm onSubmit={createChannel} >
           <AddChannelInput 
             placeholder='Add Channel Name' 
-            onChange={handleGetChannel} 
-            value = {addChannel}
-          />    
-          </AddChannelForm>
-          <AddMemberChannelForm 
-          // onSubmit = {AddMemberChannel} 
-          >
-              <AddMemberChannelSearch
-                placeholder='Add Member'
-                onChange = {handleAddMemberChannel}
-              />
-          </AddMemberChannelForm>
-          {/* <Link to="/addChannelMember" >Add Members</Link> */}
+            onChange={handleChannelInput} 
+            value = {channelName}
+          />
+          <button onClick={createChannel}>Next</button>
+          <ErrorStyle>{errorWarning ? <p>{errorText}</p> : "" }</ErrorStyle>
+        </AddChannelForm>
+        {/* <AddMemberChannelForm>
+        </AddMemberChannelForm> */}
       </AddChannelContainer>
   </AddChannelOuterContainer>
   )
@@ -159,45 +70,51 @@ const AddChannelOuterContainer = styled.div`
 
 const AddChannelContainer = styled.div`
 z-index: 2;
-height: 40vh;
-width: 30vw;
-flex-direction: column;
-background-color: whitesmoke;
-font-size: 1vh;
-box-shadow: 0 1px 3px rgba(219, 219, 219, 0.9), 0 1px 2px rgba(199, 194, 194, 0.75);
-border-radius: 2vh;
-display: flex;
-
+  height: 40vh;
+  width: 30vw;
+  flex-direction: column;
+  background-color: whitesmoke;
+  font-size: 1vh;
+  box-shadow: 0 1px 3px rgba(219, 219, 219, 0.9), 0 1px 2px rgba(199, 194, 194, 0.75);
+  border-radius: 2vh;
+  display: flex;
 `
 const AddChannelForm = styled.form`
 margin: auto;
-display: flex;
-width: 80%;
+  display: flex;
+  flex-direction: column;
+  width: 80%;
 `
 
 const AddChannelInput = styled.input`
-width: 100%;
-padding: 1.5vh;
-height: 2vh;
-margin: auto;
-border-radius: 2vh;
-border:1px solid black;
-font-size: 2vh;
+  width: 100%;
+  padding: 1.5vh;
+  height: 2vh;
+  margin: auto;
+  border-radius: 2vh;
+  border:1px solid black;
+  font-size: 2vh;
 `
 
 const AddMemberChannelForm = styled.form`
-margin: auto;
-display: flex;
-width: 80%;
-padding: 2vh;
+  margin: auto;
+  display: flex;
+  width: 80%;
+  padding: 2vh;
 `
 
 const AddMemberChannelSearch = styled.input`
-width: 100%;
-padding: 1.5vh;
-height: 2vh;
-margin: auto;
-border-radius: 2vh;
-border:1px solid black;
-font-size: 2vh;
+  width: 100%;
+  padding: 1.5vh;
+  height: 2vh;
+  margin: auto;
+  border-radius: 2vh;
+  border:1px solid black;
+  font-size: 2vh;
+`
+
+const ErrorStyle = styled.div`
+  > p {
+    color: red;
+  }
 `
